6a23461ca01ffb0080936d294b2203ff505e986e
slampham <spencerpham07@gmail.com>
2020-03-12 10:55:29 -0700

    Updated ReplayBuffer

--- dqn.py
+++ dqn.py
@@ -23,36 +23,35 @@ class QLearner(nn.Module):
         self.input_shape = self.env.observation_space.shape
         self.num_actions = self.env.action_space.n
 
-        self.features = nn.Sequential(
-            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),
+        self.features = nn.Sequential(                                  # 3 layers
+            nn.Conv2d(self.input_shape[0], 32, kernel_size=8, stride=4),    # Input 1st layer
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),                 # In --> 32, Out --> 64
             nn.ReLU(),
             nn.Conv2d(64, 64, kernel_size=3, stride=1),
             nn.ReLU()
         )
 
-        self.fc = nn.Sequential(
-            nn.Linear(self.feature_size(), 512),
+        self.fc = nn.Sequential(                    # Output layer
+            nn.Linear(self.feature_size(), 512),    # TODO: what is 'feature_size'?
             nn.ReLU(),
-            nn.Linear(512, self.num_actions)
+            nn.Linear(512, self.num_actions)        # Output num_actions
         )
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.features(x)            # Pass in state?
+        x = x.view(x.size(0), -1)       # Reshape x
+        x = self.fc(x)                  # Pass in features onto last layer
         return x
 
     def feature_size(self):
         return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
 
-    def act(self, state, epsilon):
+    def act(self, state, epsilon):  # Neg exp curve. Start exploring then exploit
         if random.random() > epsilon:
             state = Variable(torch.FloatTensor(np.float32(state)).unsqueeze(0), requires_grad=True)
-            action = torch.argmax(self(state)).item()  # FIXME
-
-            # TODO: Given state, you should write code to get the Q value and chosen action. JUST 1 LINE
+            # TODO: Why self(state) calls forward()?
+            action = torch.argmax(self(state)).item()  # TODO: Given state, write code to get Q value and chosen action
         else:
             action = random.randrange(self.env.action_space.n)
         return action
@@ -68,10 +67,15 @@ def compute_td_loss(model, target_model, batch_size, gamma, replay_buffer):
     next_state = Variable(torch.FloatTensor(np.float32(next_state)).squeeze(1), requires_grad=True)
     action = Variable(torch.LongTensor(action))
     reward = Variable(torch.FloatTensor(reward))
-    done = Variable(torch.FloatTensor(done))
+    done = Variable(torch.FloatTensor(done))    # 'done' is float to simplify Qn equation
 
     # TODO: implement the loss function here
-    loss = pow(reward + gamma * max(next_state) - state, 2)
+    Qn = reward + (1 - done) * gamma * torch.max(target_model(next_state))
+
+    Q = model(state)[action]
+
+    MSE = nn.MSELoss()
+    loss = MSE(Qn, Q)
 
     return loss
 
@@ -80,14 +84,21 @@ class ReplayBuffer(object):
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
 
-    def push(self, state, action, reward, next_state, done):
-        state = np.expand_dims(state, 0)
-        next_state = np.expand_dims(next_state, 0)
+    def push(self, state, action, reward, next_state, done):    # Done: bool
+        state = np.expand_dims(state, 0)                # Expand row-wise
+        next_state = np.expand_dims(next_state, 0)      # Expand row-wise
 
-        self.buffer.append((state, action, reward, next_state, done))
+        self.buffer.append((state, action, reward, next_state, done))   # Insert tuple of Q
 
-    def sample(self, batch_size):
+    def sample(self, batch_size):  # Initially, frame_idx = replay_buffer.len. But, sampling reduces replay_buffer size
         # TODO: Randomly sampling data with specific batch size from the buffer
+        batch = random.sample(self.buffer, batch_size)
+
+        state = [frame[0] for frame in batch]
+        action = [frame[1] for frame in batch]
+        reward = [frame[2] for frame in batch]
+        next_state = [frame[3] for frame in batch]
+        done = [frame[4] for frame in batch]
 
         return state, action, reward, next_state, done
 
--- run_dqn_pong.py
+++ run_dqn_pong.py
@@ -13,30 +13,34 @@ import torch.nn.functional as F
 USE_CUDA = torch.cuda.is_available()
 from dqn import QLearner, compute_td_loss, ReplayBuffer
 
+# Set up game
 env_id = "PongNoFrameskip-v4"
 env = make_atari(env_id)
 env = wrap_deepmind(env)
 env = wrap_pytorch(env)
 
-num_frames = 1000000
+# CONSTS
+num_frames = 1000000    # How many frames to play
 batch_size = 32
 gamma = 0.99
-record_idx = 10000
+record_idx = 10000      # FIXME: UNUSED const
 
-replay_initial = 10000
-replay_buffer = ReplayBuffer(100000)
-model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
-model.load_state_dict(torch.load("model_pretrained.pth", map_location='cpu'))
+replay_initial = 10000  # FIXME: dont know what this is
+replay_buffer = ReplayBuffer(100000)                                            # Buffer size
+model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)             # Create model
+model.load_state_dict(torch.load("model_pretrained.pth", map_location='cpu'))   # Load model with vals
 
-target_model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)
+target_model = QLearner(env, num_frames, batch_size, gamma, replay_buffer)      # Create target model
 target_model.copy_from(model)
 
+# Optimize model's parameters
 optimizer = optim.Adam(model.parameters(), lr=0.00001)  # FIXME: Maybe change LR?
 if USE_CUDA:
     model = model.cuda()
     target_model = target_model.cuda()
     print("Using cuda")
 
+# Neg exp func. Start exploring then exploiting according to frame_indx
 epsilon_start = 1.0
 epsilon_final = 0.01
 epsilon_decay = 30000
@@ -47,40 +51,40 @@ losses = []
 all_rewards = []
 episode_reward = 0
 
-state = env.reset()
+state = env.reset()  # Initial state
 
-for frame_idx in range(1, num_frames + 1):
+for frame_idx in range(1, num_frames + 1):  # Each frame in # frames played
     # print("Frame: " + str(frame_idx))
 
-    epsilon = epsilon_by_frame(frame_idx)
-    action = model.act(state, epsilon)
+    epsilon = epsilon_by_frame(frame_idx)   # Epsilon decreases as frames played
+    action = model.act(state, epsilon)      # if (rand < e) explore. Else action w max(Q-val). action: int
 
-    next_state, reward, done, _ = env.step(action)
-    replay_buffer.push(state, action, reward, next_state, done)
+    next_state, reward, done, _ = env.step(action)  # Get env info after taking action. next_state: 2d int. reward: float. done: bool.
+    replay_buffer.push(state, action, reward, next_state, done)  # Save next_state onto buffer (note: every frame)
 
-    state = next_state
-    episode_reward += reward
+    state = next_state                      # Change to next state
+    episode_reward += reward                # Keep adding rewards until goal state
 
-    if done:
-        state = env.reset()
-        all_rewards.append((frame_idx, episode_reward))
+    if done:                                # Goal state
+        state = env.reset()                 # Restart game
+        all_rewards.append((frame_idx, episode_reward))  # Store episode_reward w frame it ended
         episode_reward = 0
 
-    if len(replay_buffer) > replay_initial:
+    if len(replay_buffer) > replay_initial:     # If enough frames in replay_buffer (10000)
         loss = compute_td_loss(model, target_model, batch_size, gamma, replay_buffer)
-        optimizer.zero_grad()
+        optimizer.zero_grad()                   # Resets gradient after every mini-batch
         loss.backward()
         optimizer.step()
         losses.append((frame_idx, loss.data.cpu().numpy()))
 
-    if frame_idx % 10000 == 0 and len(replay_buffer) <= replay_initial:
+    if frame_idx % 10000 == 0 and len(replay_buffer) <= replay_initial:  # If frames still needed in replay_buffer
         print('#Frame: %d, preparing replay buffer' % frame_idx)
 
-    if frame_idx % 10000 == 0 and len(replay_buffer) > replay_initial:
+    if frame_idx % 10000 == 0 and len(replay_buffer) > replay_initial:   # If enough frames in replay_buffer
         print('#Frame: %d, Loss: %f' % (frame_idx, np.mean(losses, 0)[1]))
         print('Last-10 average reward: %f' % np.mean(all_rewards[-10:], 0)[1])
 
     if frame_idx % 50000 == 0:
-        target_model.copy_from(model)
+        target_model.copy_from(model)       # Copy model's weights onto target after 50000 frames
 
     # FIXME: torch.save
